1704cd5874fd9cb6179226d8477571433ea23d7e
hosamshaban <97hosams@gmail.com>
2023-05-18 13:13:32 +0300

    Initial commit

--- android/app/src/main/kotlin/com/example/finalproject23/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.finalproject23
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+main(){
   runApp(const MyApp());
 }
 
